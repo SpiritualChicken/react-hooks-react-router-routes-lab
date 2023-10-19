ae2336d237b39c10f630e31037d5a161fc61421f
Charlie <cnewlinds@gmail.com>
2023-10-19 23:53:04 +1100

    test error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,9 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink exact to="/" />
+      <NavLink to="/directors" />
+      <NavLink to="/actors" /> 
     </nav>
     );
 };
